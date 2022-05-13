2f1b544f5fabd795b8e599440f2a96a2d116aa0b
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-13 19:15:46 +0530

    Welcome LineComparision

---
+++ OopsLineComparision/src/com/bridgelabz/OopsLineComparision/OopsLineComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.OopsLineComparision;
+
+public class OopsLineComparision {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Program");
+	}
+
+}
